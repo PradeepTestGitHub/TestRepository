56dad2b4c2440e422ab9ba54cdf80273d661335c
Pradeep <pradeep.devarasetty@yahoo.com>
2018-11-07 09:52:41 -0600

    reactApp

---
+++ reactApp/main.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+import Grid from '@material-ui/core/Grid';
+
+//ReactDOM.render(<App />, document.getElementById('app'));
+
+class App extends React.Component {
+    render() {
+       return (
+          <div>
+             <ul>
+             <li>Home</li>
+             <li>About</li>
+             <li>Contact</li>
+             </ul>
+             {this.props.children}
+          </div>
+       )
+    }
+ }
+ export default App;
+ 
+ class Home extends React.Component {
+    render() {
+       return (
+          <div>
+             <h1>Home...</h1>
+             <Grid container spacing={40}>
+     
+   </Grid>
+          </div>
+       )
+    }
+ }
+ export default Home;
+ 
+ class About extends React.Component {
+    render() {
+       return (
+          <div>
+             <h1>About...</h1>
+          </div>
+       )
+    }
+ }
+ export default About;
+ 
+ class Contact extends React.Component {
+    render() {
+       return (
+          <div>
+             <h1>Contact...</h1>
+          </div>
+       )
+    }
+ }
+ export default Contact;
+
+ ReactDOM.render((
+    <Router history = {browserHistory}>
+       <Route path = "/" component = {App}>
+          <IndexRoute component = {Home} />
+          <Route path = "home" component = {Home} />
+          <Route path = "about" component = {About} />
+          <Route path = "contact" component = {Contact} />
+       </Route>
+    </Router>
+ ), document.getElementById('app'))
